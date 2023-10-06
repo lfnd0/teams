ba7c238b6390746d76454f5d7f370f8f1e529e1f
luizfernando <luiz-fsilva37@hotmail.com>
2023-10-05 21:49:40 -0300

    Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
+import { ActivityIndicator } from 'react-native'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { ThemeProvider } from 'styled-components'
 import { Groups } from '@screens/Groups'
 import theme from './src/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }

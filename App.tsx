e72d6f9d669ea2fec2b50d9a8694e7bd768b9744
luizfernando <luiz-fsilva37@hotmail.com>
2023-10-05 21:33:55 -0300

    Utilizando tema

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { ThemeProvider } from 'styled-components';
-import { Groups } from '@screens/Groups';
-import theme from '@theme/index';
+import { ThemeProvider } from 'styled-components'
+import { Groups } from '@screens/Groups'
+import theme from './src/theme'
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Groups />
     </ThemeProvider>
-  );
+  )
 }

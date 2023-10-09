d4b343d6ff59cb2feac5cfbb2b6a374e5d3b58ff
luizfernando <luiz-fsilva37@hotmail.com>
2023-10-09 20:15:45 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,15 +2,27 @@ import { Header } from '@components/Header'
 import { Container } from './styles'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['DEVs']);
+
   return (
     <Container>
       <Header />
 
       <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
 
-      <GroupCard title='VALORANT' />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
+
+
     </Container>
   )
 }
